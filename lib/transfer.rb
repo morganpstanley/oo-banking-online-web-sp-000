ae3bd2bfa26882e0ad203abb182413d30dba33f8
Morgan Stanley <morganpstanley@gmail.com>
2019-07-16 22:37:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,9 +29,10 @@ class Transfer
   end
 
   def reverse_transfer
-    @sender.balance += @amount
-    @receiver.balance -= @amount
-    @status = "reversed"
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
   end
 
 end
