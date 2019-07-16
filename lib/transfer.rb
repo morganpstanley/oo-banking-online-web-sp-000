3d01b1affa7255f29cee7895015361ddf3b21663
Morgan Stanley <morganpstanley@gmail.com>
2019-07-16 22:51:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,7 @@ class Transfer
       @status = "complete"
     else
       @status = "rejected"
+      binding.pry
       "Transaction rejected. Please check your account balance."
     end
   end
